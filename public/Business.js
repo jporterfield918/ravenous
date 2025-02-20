c0917041c1e882f9ab7b07e043096a28972d5417
Jenna Porterfield <jennav.porterfield@gmail.com>
2025-02-20 14:45:29 -0600

    Changes to Business.js and adding BusinessList.js

--- public/Business.js
+++ public/Business.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
-function Business() {
+function Business = (props) => {
   return (
     <div className='business'>
-        <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt='pizza' />
-        <h1>MarginOtto Pizzeria</h1>
+        <div className="image-container">
+            <img src={props.imageSrc} />
+        </div>
+        <h2>{props.name}</h2>
         <div className="business-information">
             <div className='business-address'>
-                <p>Address: 1010 Paddington Way</p>
-                <p>City: Flint</p>
-                <p>State: MI</p>
-                <p>Zip Code: 48504</p>
+                <p>{props.address}</p>
+                <p>{props.city}</p>
+                <p>{props.state}</p>
+                <p>{props.zipCode}</p>
             </div>
             <div className='business-reviews'>
-                <h3>Reviews</h3>
-                <h3>3.5 stars</h3>
-                <p>Based on 90 reviews</p>
+                <h3>{props.category}</h3>
+                <h3>{props.rating}</h3>
+                <p>Based on {props.reviewCount} reviews</p>
             </div>
         </div>
     </div>
   );
 }
+
+export default Business;
---
+++ public/BusinessList.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Business from './Business';
+
+const businesses = [
+    {
+        imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flint',
+        state: 'MI',
+        zipCode: '48504',
+        category: 'Italian',
+        rating: '4.5 stars',
+        reviewCount: 90
+    },{
+        imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flint',
+        state: 'MI',
+        zipCode: '48504',
+        category: 'Italian',
+        rating: '4.5 stars',
+        reviewCount: 90
+    },{
+        imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flint',
+        state: 'MI',
+        zipCode: '48504',
+        category: 'Italian',
+        rating: '4.5 stars',
+        reviewCount: 90
+    }
+];
+
+const BusinessList = () => {
+    return (
+        <div className="BusinessList">
+            {
+                businesses.map(business => {
+                    return <Business business={business} />;
+                })
+            }
+        </div>
+    );
+}
+
+export default BusinessList;
+// In the code above, we created a new component called BusinessList. This component is responsible for rendering a list of Business components. The BusinessList component is a functional component that returns a div with the className 'BusinessList'. Inside the div, we use the map method to iterate over the businesses array. For each business object in the array, we return a Business component with the business object as a prop. Finally, we export the BusinessList component.
