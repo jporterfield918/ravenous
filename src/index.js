0930b2a514d868c05ec747b4c6cc1b68b8f0dd20
Jenna Porterfield <jennav.porterfield@gmail.com>
2025-02-26 12:56:28 -0600

    Ugh... idk

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello World</h1>>);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
